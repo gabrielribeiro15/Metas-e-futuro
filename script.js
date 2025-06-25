b355c0fafe5bf3ec5f59a6af455a2598113b5e75
gabrielribeiro15 <141328194+gabrielribeiro15@users.noreply.github.com>
2025-06-25 10:46:15 -0300

    perguntas metas

---
+++ script.js
@@ -0,0 +1,14 @@
+const perguntas = [
+    {
+        enunciado:"1 Quando você pensa em futuro, você",
+        alternativas: [
+            {
+            texto:"Tem objetivos bem definidos",
+            afirmação:"Tem objetivos bem definidos"
+            },
+            {
+                texto:"Prefere deixar as coisas acontecerem naturalmente",
+                afirmação:"Preferiu deixar tudo para depois assim perdeu varias oportunidades na vida"
+                
+            }
+        
