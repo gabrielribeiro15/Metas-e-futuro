f0405661ab154abd25adc7023ceff77f0accb1e5
gabrielribeiro15 <141328194+gabrielribeiro15@users.noreply.github.com>
2025-07-02 10:45:14 -0300

    .

--- script.js
+++ script.js
@@ -11,4 +11,71 @@ const perguntas = [
                 afirmação:"Preferiu deixar tudo para depois assim perdeu varias oportunidades na vida"
                 
             }
-        
+]
+},
+{
+        enunciado:"Para alcançar uma meta difícil, você",
+        alternativas: [
+            {
+            texto:"Traça um plano detalhado e o segue.",
+            afirmação:"Traça um plano detalhado e o segue."
+            },
+            {
+                texto:"Vai se adaptando conforme os desafios aparecem.",
+                afirmação:"Vai se adaptando conforme os desafios aparecem."
+             }
+]
+},
+{               
+        enunciado:"Em relação às suas ambições, você:",
+        alternativas: [
+            {
+            texto:"Tem objetivos bem definidos",
+            afirmação:"Tem objetivos bem definidos"
+            },
+            {
+                texto:"Prefere guardar para si.",
+                afirmação:"Prefere guardar para si."
+            }
+]
+},
+{          
+        enunciado:"Quando falha ao tentar algo importante:"
+        alternativas: [
+            {
+            texto:"Reavalia e tenta de novo com mais força.",
+            afirmação:"Reavalia e tenta de novo com mais força."
+            },
+            {
+                texto:"Pensa que talvez não fosse pra ser.",
+                afirmação:"Pensa que talvez não fosse pra ser."
+            }
+]
+},
+{             
+        enunciado:"Você se sente mais motivado por:",
+        alternativas: [
+            {
+            texto:"Conquistar algo grande no futuro.",
+            afirmação:"Conquistar algo grande no futuro."
+            },
+            {
+                texto:"Aproveitar bem o presente.",
+                afirmação:"Aproveitar bem o presente."
+                
+            }
+             }
+]
+},
+{                    
+        enunciado:"Sobre metas de longo prazo:",
+        alternativas: [
+            {
+            texto:"Já tem uma lista ou visão clara.",
+            afirmação:"Já tem uma lista ou visão clara."
+            },
+            {
+                texto:"Ainda está tentando descobrir o que realmente quer.",
+                afirmação:"Ainda está tentando descobrir o que realmente quer."
+                
+            }
